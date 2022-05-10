e42f3a6a60b1f664fa683978cf051013e8a6551d
Kevin Jansen <kevitius99@gmail.com>
2022-05-10 22:18:08 +0200

    Enabled TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('app', './assets/js/app.ts')
     .addStyleEntry('main', './assets/css/main.css')
-
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -64,7 +61,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // Vue loader
-    .enableVueLoader()
+    .enableVueLoader(() => {}, { runtimeCompilerBuild: false })
 
     // PostCSS
     .enablePostCssLoader(options => {
